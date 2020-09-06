805061473ef974c002f8fb680b9bf6af3a694607
Gabriel <gabriel_g_alves@hotmail.com>
2020-09-06 13:55:03 +0100

    jwt development 2

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: "kasij1uh321ujo23i21h3u21h",
+    expiresIn: "1d",
+  },
+};
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -2,6 +2,7 @@ import { getRepository } from "typeorm";
 import { hash, compare } from "bcryptjs";
 import { sign } from "jsonwebtoken";
 import User from "../models/User";
+import authConfig from "../config/auth";
 
 interface RequestDTO {
   email: string;
@@ -27,9 +28,11 @@ class AuthenticateUserService {
       throw new Error("Incorrect email/password combination");
     }
 
-    const token = sign({}, "kasij1uh321ujo23i21h3u21h", {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: "1d",
+      expiresIn: expiresIn,
     });
 
     return {
